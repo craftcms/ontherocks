c19009b4e5d046a34608094a8a814a51891cfda8
brandonkelly <brandon@pixelandtonic.com>
2018-09-24 03:25:59 +0200

    Create a module

---
+++ src/Module.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ontherocks;
+
+use Craft;
+
+/**
+ * Custom module class.
+ *
+ * This class will be available throughout the system via:
+ * `Craft::$app->getModule('my-module')`.
+ *
+ * You can change its module ID ("my-module") to something else from
+ * config/app.php.
+ *
+ * If you want the module to get loaded on every request, uncomment this line
+ * in config/app.php:
+ *
+ *     'bootstrap' => ['my-module']
+ *
+ * Learn more about Yii module development in Yii's documentation:
+ * http://www.yiiframework.com/doc-2.0/guide-structure-modules.html
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * Initializes the module.
+     */
+    public function init()
+    {
+        // Set a @modules alias pointed to the modules/ directory
+        Craft::setAlias('@ontherocks', __DIR__);
+
+        // Set the controllerNamespace based on whether this is a console or web request
+        if (Craft::$app->request->isConsoleRequest) {
+            $this->controllerNamespace = __NAMESPACE__ . '\\console\\controllers';
+        } else {
+            $this->controllerNamespace = __NAMESPACE__ . '\\web\\controllers';
+        }
+
+        parent::init();
+    }
+}
